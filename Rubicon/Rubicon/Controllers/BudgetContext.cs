f8f39038a1d72cf487a7b3d1e63429b5ed77d90f
Diana001a <82045854+Diana001a@users.noreply.github.com>
2021-09-19 17:51:05 +0300

    1

---
+++ Rubicon/Rubicon/Controllers/BudgetContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rubicon.Models;
+
+namespace Rubicon.Controllers
+{
+    public class BudgetContext : DbContext 
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            :base(options)
+            {
+            }
+        public DbSet<Budget> Budgets { get; set; }
+    }
+            
+       
+}
+
---
+++ Rubicon/Rubicon/Models/Budget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubicon.Models
+{
+    public class Budget
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Label_percentage
+        {
+            get => (Percentage * 100).ToString() + "%";
+        }
+
+        public float Percentage { get; set; }
+
+        public string Color { get; set; }
+    }
+}
